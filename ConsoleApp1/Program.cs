09c25dcd8ef0dea24b7ffe9e6c5f3c2348425b13
cemen2009 <98355726+cemen2009@users.noreply.github.com>
2024-11-07 11:31:32 +0200

    creating DLL

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,46 +1,18 @@
 ﻿using Laba01;
-
-namespace BasicOOP
-{
-    public class Body
-    {
-        public Body()
-        {
-
-        }
-
-        public virtual float GetCalculatedArea()
-        {
-            Console.WriteLine("Area calculations are not provided");
-            return 0;
-        }
-    }
-
-    public class Sphere : Body
-    {
-        private float radius;
-
-        public Sphere(float radius)
-        {
-            this.radius = radius;
-        }
-
-        public override float GetCalculatedArea()
-        {
-            return 4 * (float)Math.PI * radius * radius;
-        }
-    }
-}
-
+using MathOperations;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
-        Sphere sphere = new Sphere(2f);
-        Parallelepiped parallelepiped = new Parallelepiped(1f, 3.5f, 1.2f);
-        Tetrahedron tetrahedron = new Tetrahedron(3f);
+        // Sphere sphere = new Sphere(2f);
+        // Parallelepiped parallelepiped = new Parallelepiped(1f, 3.5f, 1.2f);
+        // Tetrahedron tetrahedron = new Tetrahedron(3f);
 
-        Console.WriteLine($"sphere volume: {sphere.GetCapacity()}\nparallelepiped volume: {parallelepiped.GetCapacity()}\ntetrahedron volume: {tetrahedron.GetCapacity()}");
+        // Console.WriteLine($"sphere volume: {sphere.GetCapacity()}\nparallelepiped volume: {parallelepiped.GetCapacity()}\ntetrahedron volume: {tetrahedron.GetCapacity()}");
+
+        var _operator = new MathOperations.MathOperations();
+
+        Console.WriteLine($"factorial 5: {_operator.Factorial(5)}\ntriangle: a = 5, b = 3.5, c = 6\nS = {_operator.CalculateTriangleArea(5, 3.5f, 6)}");
     }
 }
---
+++ MathOperations/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MathOperations
+{
+    public class MathOperations
+    {
+        public int Factorial(int number)
+        {
+            int maxIteration = number;
+            int result = 1;
+
+            for (int i = 1; i <= maxIteration; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        public double CalculateTriangleArea(float a, float b, float c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0) return -1;
+
+            
+            float p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
